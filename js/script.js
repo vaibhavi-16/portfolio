a66c2a0f861250a703ad7187872f9c885d65c5bc
VAIBHAVI KUMARI <73634081+vaibhavi-16@users.noreply.github.com>
2022-01-04 18:47:21 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Fresher in ","Python","Machine Learning","Front-end Development","Natural Language Processing","Pandas","MySQL"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
